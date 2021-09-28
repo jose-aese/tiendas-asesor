16c14148608848b5d9f0bdaa20be777aa378c9b2
JoseP <jose.perezar@elektra.com.mx>
2021-09-28 15:17:56 -0500

    Add Tienda Service Get

--- src/app/services/tiendas.service.ts
+++ src/app/services/tiendas.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
+import { concatMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TiendasService {
   private urlService = environment.ws;
-  constructor(public http: HttpClient) {
+  constructor(public http: HttpClient) {}
+
+  tiendas(numeroEmpleado: number): any {
+    let headers = new HttpHeaders().set(
+      'Content-Type',
+      'application/x-www-form-urlencoded'
+    );
+    const url = this.urlService + '/asesores/' + numeroEmpleado;
+
+    return from([1]).pipe(
+      concatMap((id) => {
+        return <Observable<any>>this.http.get(url, { headers });
+      })
+    );
   }
-
-  tiendas(numeroEmpleado:number): Observable<any> {
-    let headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');
-    const url = this.urlService + '/asesores/'+numeroEmpleado;
-    return this.http.get(url,{headers});
-  }
-
-  
-
 }
